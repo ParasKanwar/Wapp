99b6d27d1e9730f98c39f8fdd382b24626b2a387
paras <paraskanwar30@gmail.com>
2019-07-18 20:01:09 +0530

    commiting this time

--- src/Utils/forecast.js
+++ src/Utils/forecast.js
@@ -1,14 +1,17 @@
-const request = require("request");
-const TokenUrl =
-  "https://api.darksky.net/forecast/1301187e8510188ce3cb6497fd69a1f2/";
+const request = require('request')
+
 const forecast = (latitude, longitude, callback) => {
-  request(
-    { uri: TokenUrl + latitude + "," + longitude + "?units=si", json: true },
-    (error, response) => {
-      if (error) callback("Can't Connect to Weather Services");
-      else if (response.body.error) callback(response.body.error);
-      else callback(undefined, response);
-    }
-  );
-};
-module.exports = forecast;
+    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
